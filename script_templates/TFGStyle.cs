b790492aaf2b45bbc37fc435d94d001b8545ee26
Thomas Hinkle <thomashinkle2393@gmail.com>
2020-11-08 12:51:21 -0600

    added the C# template.

---
+++ script_templates/TFGStyle.cs
@@ -0,0 +1,25 @@
+using Godot;
+using System;
+
+public class %CLASS% : %BASE%
+{
+	// OnReady Variables
+	
+	// Exported Variables
+	
+	// Constant Variables
+	
+	// Class Variables
+	
+	// Override Methods
+	public override void _Ready() 
+	{
+	
+	}
+	
+	// Private Methods
+	
+	// Public Methods
+	
+	// Signal Methods
+}
