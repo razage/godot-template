cd41c5e523fe743d6cf6449f23554fe6164d30ca
Thomas Hinkle <thomashinkle2393@gmail.com>
2020-11-08 12:01:18 -0600

    initial commit

---
+++ script_templates/tfg_style.gd
@@ -0,0 +1,22 @@
+extends %BASE%
+
+# class_name x
+
+# OnReady Variables
+
+# Exported Variables
+
+# Constant Variables
+
+# Class Variables
+
+# Override Methods
+
+func _ready()%VOID_RETURN%:
+	pass
+
+# Private Methods
+
+# Public Methods
+
+# Signal Methods
